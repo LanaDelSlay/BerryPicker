9419ed3998d6b338c49b817b917c427975b7b022
graham <graham@localhost>
2020-05-25 17:40:21 -0700

    Tidying!

--- test/pokeberryClassTest.java
+++ test/pokeberryClassTest.java
@@ -1,18 +1,12 @@
 import static org.junit.Assert.assertEquals;
-
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
-
 import org.json.JSONObject;
-import org.junit.Assert.*;
-
 import org.junit.Test;
 
+@SuppressWarnings("deprecation")
 public class pokeberryClassTest {
-
 	Pokeberry pb = new Pokeberry();
-	
 	
 	@Test
 	public void testNameMutator() {
@@ -20,22 +14,20 @@ public class pokeberryClassTest {
 	assertEquals("blue" , pb.getName());	
 	}
 
-	@SuppressWarnings("deprecation")
 	@Test
-	
 	public void testEfficiency() {
 		pb.setSize(500);
 		pb.setSpeed(9);
 		assertEquals(55, pb.getEfficiency(),1e-3);
 	}
-	@Test
 	
+	@Test
 	public void testQuery() throws IOException {
-		String name = "skitty"; //ID is 300
+		String name = "skitty";
 		int baseXP = 52;
 		int weight = 110;
 		int height = 6;
-		JSONObject testJSON = Pokeberry.queryForJson("/pokemon/300");
+		JSONObject testJSON = Pokeberry.queryForJson("/pokemon/300"); //Skitty's ID, 300.
 		
 		int baseXpFromJSON = (int) testJSON.query("/base_experience");
 		String nameFromJSON = (String) testJSON.query("/name");
@@ -47,21 +39,23 @@ public class pokeberryClassTest {
 		assertEquals(baseXP, baseXpFromJSON);
 		assertEquals(height, heightFromJSON);
 	}
-	@Test
 	
+	@Test
 	public void testSorting() {
 		Pokeberry myBerries = new Pokeberry();
 		Pokeberry godBerries = new Pokeberry();
+		
 		myBerries.setName("Sucky berries"); myBerries.setSize(35); myBerries.setSpeed(15);
 		godBerries.setName("Dank berries"); godBerries.setSize(3500); godBerries.setSpeed(5);
 		ArrayList<Pokeberry> arrayList = new ArrayList<Pokeberry>();
+		
 		arrayList.add(myBerries);
 		arrayList.add(godBerries);
 		arrayList.sort(null);
 		assertEquals("Dank berries", arrayList.get(0).getName());
 	}
-	@Test
 	
+	@Test
 	public void testNameCapitalization() {
 		pb.setName("punk");
 		assertEquals("Punk", pb.getNameCapped());
